fe9d0e9aaee314c103543729d6623829a8714482
Riccardo Ferrari <richi.f93@gmail.com>
2021-01-12 16:20:28 +0100

    second commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
-var app = new Veu ({
+var app = new Vue ({
   el: "#root",
   data: {
-    
+    agencyPhoneNumbers: ["(001) 88451234", "88455438"],
+    socialIcons: ["fab fa-facebook-f", "fab fa-twitter", "fab fa-pinterest-p"],
+    agencyLogo: "img/cropped-Group-39-2x.png",
+    navbarLinks: ["HOME", "ABOUT", "SERVICES", "SHOWCASE", "BLOG", "CONTACT"],
+    socialLinks: ["FACEBOOK", "INSTAGRAM", "YOUTUBE", "TWITTER"],
+    slider: [
+      {
+        preTitle: " 17 YEARS OF EXPERIENCE",
+        title: "WE ARE A WEB DESIGN AGENCY",
+        paragraph: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the semantics, a large language ocean. Separated they live in Bookmarksgrove."
+      },
+      {
+        preTitle: " 17 YEARS OF EXPERIENCE",
+        title: "WE ARE A WEB DESIGN AGENCY",
+        paragraph: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the semantics, a large language ocean. Separated they live in Bookmarksgrove."
+      },
+      {
+        preTitle: " 17 YEARS OF EXPERIENCE",
+        title: "WE ARE A WEB DESIGN AGENCY",
+        paragraph: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the semantics, a large language ocean. Separated they live in Bookmarksgrove."
+      }
+    ]
+
+
   }
 })
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@
 
 let mix = require('laravel-mix');
 
-// mix.js('src/app.js', 'dist').setPublicPath('dist');
+
 mix.sass('src/app.scss', 'css');
