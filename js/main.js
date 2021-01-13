9880f4f021cc3d2b83957e499ebc455366028483
Riccardo Ferrari <richi.f93@gmail.com>
2021-01-13 10:06:28 +0100

    header and first main

--- js/main.js
+++ js/main.js
@@ -9,21 +9,40 @@ var app = new Vue ({
     slider: [
       {
         preTitle: " 17 YEARS OF EXPERIENCE",
-        title: "WE ARE A WEB DESIGN AGENCY",
-        paragraph: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the semantics, a large language ocean. Separated they live in Bookmarksgrove."
+        title: "WE ARE A <br> WEB DESIGN <span>AGENCY</span>",
+        paragraph: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the semantics, a large language ocean. Separated they live in Bookmarksgrove.",
+      },
+      {
+        preTitle: " 17 YEARS OF EXPERIENCE",
+        title: "ososososososos",
+        paragraph: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the semantics, a large language ocean. Separated they live in Bookmarksgrove.",
+
       },
       {
         preTitle: " 17 YEARS OF EXPERIENCE",
         title: "WE ARE A WEB DESIGN AGENCY",
-        paragraph: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the semantics, a large language ocean. Separated they live in Bookmarksgrove."
-      },
-      {
-        preTitle: " 17 YEARS OF EXPERIENCE",
-        title: "WE ARE A WEB DESIGN AGENCY",
-        paragraph: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the semantics, a large language ocean. Separated they live in Bookmarksgrove."
+        paragraph: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the semantics, a large language ocean. Separated they live in Bookmarksgrove.",
       }
-    ]
+    ],
+    sliderImages: ["img/Group-36-2x.png", "img/Group-35-2x.png", "img/Group-40-2x.png"],
+    chooseImageNumbers: ["01", "02", "03"],
+    indexImages: 0,
+    indexLinks: 0,
 
 
+  },
+  methods: {
+
+    chooseImage: function(index) {
+      this.indexImages = index;
+
+      // SLIDER---------------------------------------
+    },
+    activeLinks: function(index) {
+      this.indexLinks = index;
+    }
+    // activeFucntion: function {
+    //
+    // }
   }
 })
